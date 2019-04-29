928cef5d99415d4adfc9f50346a3bfbb8b3d42aa
Norah Say <norahsay@gmail.com>
2019-04-28 20:33:48 -0700

    Add AlbumList and AlbumDetail components

---
+++ src/components/AlbumList.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import AlbumDetail from './AlbumDetail'
+
+class AlbumList extends Component {
+  //Step 1 : Setting initial state
+  constructor(props) {
+    super(props);
+    this.state = { albums: [] };
+  }
+
+  componentWillMount() {
+    fetch('https://rallycoding.herokuapp.com/api/music_albums')
+      .then((response) => response.json())
+      .then((responseData) => {
+        //Step2: Updating albums
+        this.setState({ albums: responseData });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+  renderAlbums() {
+    return this.state.albums.map(album =>
+      <AlbumDetail key={album.title} album={album} />
+    );
+  }
+
+  render() {
+    return (
+      <View>
+        { this.renderAlbums() }
+      </View>
+    );
+  }
+}
+
+export default AlbumList;
